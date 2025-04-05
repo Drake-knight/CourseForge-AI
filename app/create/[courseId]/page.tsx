ed396e47dd622c553586b3fccccb41793597c79c
Tejas-sarvam <tejas@sarvam.ai>
2025-04-05 23:11:06 +0530

    pages for each route done

---
+++ app/create/[courseId]/page.tsx
@@ -0,0 +1,52 @@
+import ConfirmChapters from "@/components/ConfirmChapters";
+import { getCurrentUserSession } from "@/lib/auth";
+import { prisma } from "@/lib/db";
+import { Info } from "lucide-react";
+import { redirect } from "next/navigation";
+import React from "react";
+
+type Props = {
+  params: {
+    courseId: string;
+  };
+};
+
+const CreateChapters = async ({ params: { courseId } }: Props) => {
+  const session = await getCurrentUserSession();
+  if (!session?.user) {
+    return redirect("/gallery");
+  }
+  const course = await prisma.course.findUnique({
+    where: {
+      id: courseId,
+    },
+    include: {
+      units: {
+        include: {
+          chapters: true,
+        },
+      },
+    },
+  });
+  if (!course) {
+    return redirect("/create");
+  }
+  return (
+    <div className="flex flex-col items-start max-w-xl mx-auto my-16">
+      <h5 className="text-sm uppercase text-seconday-foreground/60">
+        Course Name
+      </h5>
+      <h1 className="text-5xl font-bold">{course.name}</h1>
+
+      <div className="flex p-4 mt-5 border-none bg-secondary">
+        <Info className="w-12 h-12 mr-3 text-blue-400" />
+        <div>
+        Chapters for all units have been successfully generated. Kindly review them and proceed by clicking the confirmation button
+        </div>
+      </div>
+      <ConfirmChapters course={course} />
+    </div>
+  );
+};
+
+export default CreateChapters;
